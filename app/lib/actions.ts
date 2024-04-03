e8d997281c0314da3a18c37613e0e38adac29c82
timothynn <timothynn08@gmail.com>
2024-04-03 18:55:50 +0300

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,21 +25,21 @@ const FormSchema = z.object({
 export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
-  ) {
+) {
     try {
-      await signIn('credentials', formData);
+        await signIn('credentials', formData);
     } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case 'CredentialsSignin':
-            return 'Invalid credentials.';
-          default:
-            return 'Something went wrong.';
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
         }
-      }
-      throw error;
+        throw error;
     }
-  }
+}
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
